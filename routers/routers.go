15895d49f74e098a2e4f42cda910d61ee89c230b
humorliang <humorliang@163.com>
2019-03-26 17:42:34 +0800

    add new router

--- routers/routers.go
+++ routers/routers.go
@@ -6,13 +6,16 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/humorliang/file-cms/controllers/file"
+	"net/http"
 )
 
 func RegisterRouter(r *gin.Engine) {
 	//session管理器
 	store := cookie.NewStore([]byte("secretKey"))
 	r.Use(sessions.Sessions("session_id", store))
-
+	//异常恢复中间件
+	r.Use(gin.Recovery())
+	r.StaticFS("/static/", http.Dir("."))
 	//API版本
 	rV1 := r.Group("/v1")
 
@@ -21,6 +24,9 @@ func RegisterRouter(r *gin.Engine) {
 	rV1.POST("/user/register", user.Register)
 
 	//需要认证
+	//rAuth := rV1.Group("/auth", middleware.SessionAuth())
 	rAuth := rV1.Group("/auth")
-	rAuth.POST("/file/upload",file.UplodeFile)
+	rAuth.POST("/file/upload", file.UplodeFile)
+	rAuth.GET("/files/:page_num", file.GetFileList)
+	rAuth.GET("/file/download",file.DownloadFile)
 }
