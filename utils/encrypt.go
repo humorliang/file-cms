22e2f548f5f37c0722012d318d2727858255c9a4
humorliang <humorliang@163.com>
2019-03-27 10:19:10 +0800

    add aes Encrypt and test file

--- utils/encrypt.go
+++ utils/encrypt.go
@@ -9,6 +9,9 @@ import (
 	"crypto/x509"
 	"crypto/rsa"
 	"crypto/rand"
+	"crypto/aes"
+	"crypto/cipher"
+	"bytes"
 )
 
 func NewStrToSHA256(msg string) string {
@@ -52,4 +55,103 @@ func RsaDecrypt(ciphertext []byte, privateKey string) ([]byte, error) {
 		return nil, err
 	}
 	return rsa.DecryptPKCS1v15(rand.Reader, priv, ciphertext)
+}
+
+/*
+AES:
+全称Cipher Block Chaining mode，密码分组链接模式。
+分组方式：将明文分组与前一个密文分组进行XOR运算，然后再进行加密。每个分组的加解密都依赖于前一个分组。
+而第一个分组没有前一个分组，因此需要一个初始化向量（initialization vector）。
+CBC模式+PKCS5 填充方式 实现AES加密
+*/
+
+//AES key必须是16字节 24字节 32字节中的一种
+func CheckAesKey(strKey string) []byte {
+	keyLen := len(strKey)
+	arrKey := []byte(strKey)
+	if keyLen >= 32 {
+		//取前32个字节
+		return arrKey[:32]
+	}
+	if keyLen >= 24 {
+		//取前24个字节
+		return arrKey[:24]
+	}
+	if keyLen >= 16 {
+		//取前16个字节
+		return arrKey[:16]
+	}
+	//补齐16个字节
+	tmp := make([]byte, 16)
+	for i := 0; i < 16; i++ {
+		if i < keyLen {
+			tmp[i] = arrKey[i]
+		} else {
+			tmp[i] = '0'
+		}
+	}
+	return tmp
+}
+
+//AES加密的默认向量，
+const aesIvDefValue = "217c1b45372d45c3"
+
+//AES加密
+func AesEncrypt(plaintext []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Println("err=", err)
+		return nil, errors.New("invalid decrypt key")
+	}
+	blockSize := block.BlockSize()
+	plaintext = PKCS5Padding(plaintext, blockSize)
+	iv := []byte(aesIvDefValue)
+	blockMode := cipher.NewCBCEncrypter(block, iv)
+
+	ciphertext := make([]byte, len(plaintext))
+	blockMode.CryptBlocks(ciphertext, plaintext)
+
+	return ciphertext, nil
+}
+
+//AES解密
+func AesDecrypt(ciphertext []byte, key []byte) ([]byte, error) {
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, errors.New("invalid decrypt key")
+	}
+
+	blockSize := block.BlockSize()
+
+	if len(ciphertext) < blockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	iv := []byte(aesIvDefValue)
+	if len(ciphertext)%blockSize != 0 {
+		return nil, errors.New("ciphertext is not a multiple of the block size")
+	}
+
+	blockModel := cipher.NewCBCDecrypter(block, iv)
+
+	plaintext := make([]byte, len(ciphertext))
+	blockModel.CryptBlocks(plaintext, ciphertext)
+	plaintext = PKCS5UnPadding(plaintext)
+
+	return plaintext, nil
+}
+
+//组装
+func PKCS5Padding(src []byte, blockSize int) []byte {
+	padding := blockSize - len(src)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(src, padtext...)
+}
+
+//拆分
+func PKCS5UnPadding(src []byte) []byte {
+	length := len(src)
+	unpadding := int(src[length-1])
+	return src[:(length - unpadding)]
 }
--- utils/encrypt_test.go
+++ utils/encrypt_test.go
@@ -6,6 +6,15 @@ import (
 )
 
 func TestNewStrToSHA256(t *testing.T) {
-	fmt.Printf("%x",NewStrToSHA256("test"))
-	t.Log(NewStrToSHA256("test")==NewStrToSHA256("test"))
+	fmt.Printf("%x", NewStrToSHA256("test"))
+	t.Log(NewStrToSHA256("test") == NewStrToSHA256("test"))
+}
+
+func TestAesEncryptAndAesDecrypt(t *testing.T) {
+	data := "this is data"
+	key := CheckAesKey("123123kj131k3k14")
+	encryptData, err := AesEncrypt([]byte(data), key)
+	fmt.Println("aes加密：", encryptData, err)
+	decryptData, err := AesDecrypt(encryptData, key)
+	fmt.Println("aes解密：", string(decryptData[:]), err)
 }
