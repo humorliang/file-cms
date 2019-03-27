0d9fe18dd723f3f8ae1b8adfb5a50916543d59b7
humorliang <humorliang@163.com>
2019-03-27 10:53:15 +0800

    file list pagenation

--- controllers/file/file.go
+++ controllers/file/file.go
@@ -5,13 +5,24 @@ import (
 	"github.com/humorliang/file-cms/db"
 	"github.com/humorliang/file-cms/controllers/rsp"
 	"github.com/humorliang/file-cms/comm/e"
+	"strconv"
 )
 
 //获取文件列表
 func GetFileList(c *gin.Context) {
-	files, err := db.GetFiles()
+	pageNum := c.Param("page_num")
+	num, err := strconv.Atoi(pageNum)
+	if err != nil {
+		c.JSON(400, rsp.Fails(e.PARAM_PARSE, e.GetMsg(e.PARAM_PARSE)))
+		return
+	}
+	files, err := db.GetFiles(num)
 	if err != nil {
 		c.JSON(500, rsp.Fails(e.INTERSERVER_ERROR, e.GetMsg(e.INTERSERVER_ERROR)))
+		return
+	}
+	if len(files.FList) == 0 {
+		c.JSON(200, rsp.Success("无更多数据"))
 		return
 	}
 	rspInfo := []gin.H{}
